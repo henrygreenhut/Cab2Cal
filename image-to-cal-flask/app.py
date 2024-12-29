d987ea16610ded135cb746feb9b5e8a35e26fdbb
henrygreenhut <henrysgreenhut@gmail.com>
2024-12-29 14:02:16 -0500

    no /

--- image-to-cal-flask/app.py
+++ image-to-cal-flask/app.py
@@ -14,7 +14,7 @@ client = OpenAI(
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
   "http://127.0.0.1:3000",
-  "https://ai-image-to-cal.vercel.app/",
+  "https://ai-image-to-cal.vercel.app",
 ]}})
 
 
