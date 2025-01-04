32cc5abcd8912292b1f4b27c2e937dd4c50e2fc3
henrygreenhut <henrysgreenhut@gmail.com>
2025-01-03 22:04:39 -0500

    back to open tab with link

--- image-to-cal-react/image-to-cal/src/components/CalendarLinks.jsx
+++ image-to-cal-react/image-to-cal/src/components/CalendarLinks.jsx
@@ -15,15 +15,8 @@ function CalendarLinks({ icsURL }) {
     // Direct user to Google Calendar's import page with your icsURL
     // This opens a new tab
     const googleImportURL = `https://www.google.com/calendar/render?cid=${encodeURIComponent(icsURL)}`;
-      // Open in a new tab using a small delay to mitigate mobile redirection issues
-  const newTab = window.open('', '_blank');
-  if (newTab) {
-    newTab.location.href = googleImportURL;
-  } else {
-    // Fallback for popup blockers
-    alert("Please enable pop-ups for this site to use this feature.");
-  }
-};
+    window.open(googleImportURL, '_blank');
+  };
 
   // 3. "Add to Apple Calendar" button
   //    Typically, Apple Calendar is triggered by opening/downloading the .ics file.
